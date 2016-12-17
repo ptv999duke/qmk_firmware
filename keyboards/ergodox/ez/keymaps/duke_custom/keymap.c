263d830574d47e1ef45d5579221c7add66ad8069
root <duke.dukkyeom.kim@gmail.com>
2016-12-16 23:56:15 -0800

    Switch bottom left key to be momentary switch to layer one. Remove MDIA layer. Unsuccessfully attempt to dance between momentary layer switching and layer toggling.

--- keyboards/ergodox/ez/keymaps/duke_custom/keymap.c
+++ keyboards/ergodox/ez/keymaps/duke_custom/keymap.c
@@ -9,6 +9,10 @@
 #define MDIA 3 // media keys
 #define EPRM M(1) // Macro 1: Reset EEPROM
 
+// enum {
+//   LAYER_ONE_TOGGLE = 0
+// };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -21,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| End  |             | PgDn |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |             |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'             `-------------+------+------+------+------+--------'
- *   |~L1   |  '"  |  Alt | Left | Right|                                   18898      |  Up  | Down |   [  |   ]  |   ~L2  |
+ *   |MO(L1)|  '"  |  Alt | Left | Right|                                   18898      |  Up  | Down |   [  |   ]  |   ~L2  |
  *   `----------------------------------'                                         `------------------------------------'
  *                                        ,----------------.        ,-----------------.
  *                                        | LGui |   App   |        | Alt     |  Esc  |
@@ -35,15 +39,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,    KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_FN3,
-        KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_HOME,
-        KC_LCTRL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
-        KC_LSFT,   KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_END,
-        KC_FN1,    KC_QUOT,  KC_LALT,  KC_LEFT, KC_RGHT,
+        KC_GRV,                  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_FN3,
+        KC_TAB,                  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_HOME,
+        KC_LCTRL,                KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
+        KC_LSFT,                 KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_END,
+        MO(1),    KC_QUOT,  KC_LALT,  KC_LEFT, KC_RGHT,
 
-                                                        KC_LGUI,  ALT_T(KC_APP),  
-                                                                  LCTL(KC_LSFT),
-                                               KC_SPC,  KC_BSPC,  LALT(KC_LSFT),
+                                                                        KC_LGUI,  ALT_T(KC_APP),  
+                                                                                  LCTL(KC_LSFT),
+                                                               KC_SPC,  KC_BSPC,  LALT(KC_LSFT),
         // right hand
         KC_CAPS,  KC_6,   KC_7,   KC_8,     KC_9,     KC_0,     KC_MINS,
         KC_PGUP,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
@@ -63,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |  F6  |  F7  |  F8  |  F9  |  F10 |      |           |      |   (  |   )  |   -  |   _  |   =  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  F11 |  F12 |  Up  |      |      |------|           |------|   {  |   }  |   &  |   %  |   ;  |        |
+ * |        |  F11 |  F12 |  Up  | Lclk | Rclk |------|           |------|   {  |   }  |   &  |   %  |   ;  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      | Left | Down | Right|      |      |           |      |   [  |   ]  |   ,  |   .  |   /  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------------+------+------+--------'
@@ -82,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        M(0),   KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,
        KC_TRNS,KC_F6,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,
-       KC_TRNS,KC_F11, KC_F12,   KC_UP,    KC_TRNS,  KC_TRNS,
+       KC_TRNS,KC_F11, KC_F12,   KC_UP,    KC_BTN1,  KC_BTN2,
        KC_TRNS,KC_TRNS,KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_TRNS,  KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,
 
@@ -186,7 +190,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
     [2] = ACTION_LAYER_TAP_TOGGLE(NUMPAD),
-    [3] = ACTION_LAYER_TAP_TOGGLE(MDIA),
+    [3] = ACTION_LAYER_TOGGLE(SYMB),
+    [4] = ACTION_LAYER_TOGGLE(MDIA),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -212,6 +217,17 @@ void matrix_init_user(void) {
 
 };
 
+// void momentary_layer_one(qk_tap_dance_state_t *state, void *user_data) {
+//   ACTION_LAYER_MOMENTARY(1);
+// }
+
+// void toggle_layer_one(qk_tap_dance_state_t *state, void *user_data) {
+//   ACTION_LAYER_TOGGLE(1);
+// }
+
+// qk_tap_dance_action_t tap_dance_actions[] = {
+//   [LAYER_ONE_TOGGLE]  = ACTION_TAP_DANCE_DOUBLE(momentary_layer_one, toggle_layer_one)
+// };
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
